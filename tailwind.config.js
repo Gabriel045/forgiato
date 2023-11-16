bdbcb660d1175f367bf5c9beae838987c3059c82
gabriel Cabrera <gabriel.cabera@gmail.com>
2023-11-16 11:39:24 -0400

    initializing the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{php,html,js}", "./template-parts/*.{php,html,js}"],
+  content: ["./**.{php,html,js}","./template-parts/*.{php,html,js}","./blocks/*/**.{php,html,js}"],
   theme: {
     extend: {
       fontFamily: {
